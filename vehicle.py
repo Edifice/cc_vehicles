4dfd33802558b5bf6391489847c97ce8369bf6e4
Agi Marczinko <katyvaszok@gmail.com>
2016-03-02 08:11:43 +0100

    delete count functions

--- vehicle.py
+++ vehicle.py
@@ -1,9 +1,4 @@
 class Vehicle(object):
-
-  count = 0
-
-  def __init__(self, registration_number):
-    self.registration_number = registration_number
-    count += 1
-
-
+    
+    def __init__(self, registration_number):
+        self.registration_number = registration_number
