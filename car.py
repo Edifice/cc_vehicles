234d451747adf2e9b225c6df1b5685c0dae06433
Agi Marczinko <katyvaszok@gmail.com>
2016-03-02 08:18:28 +0100

    make car subclass

---
+++ car.py
@@ -0,0 +1,7 @@
+from vehicle import Vehicle
+
+class Car(Vehicle):
+
+	def __init__(self, registration_number, number_of_seats):
+		super(Car, self).__init__(registration_number)
+		self.number_of_seats = number_of_seats
