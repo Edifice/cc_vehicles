0cb58291239b5ddd6052bfc9b4a14d29d476fe70
Marczinkó Ágnes <katyvaszok@gmail.com>
2016-03-07 17:10:32 +0100

    Delete str function in motorcycle subclass

--- motorcycle.py
+++ motorcycle.py
@@ -5,7 +5,4 @@ class Motorcycle(Vehicle):
 	def __init__(self, registration_number, acceleration):
 		super(Motorcycle, self).__init__(registration_number)
 		self.acceleration = acceleration
-
-	def __str__(self):
-		return 'motorcycle|{}|{}'.format(self.registration_number, self.acceleration)
 		
