949a3187309722793bfc7742cad6c712df963524
Agi Marczinko <katyvaszok@gmail.com>
2016-03-10 22:15:55 +0100

    Fix obejct count function

--- vehicle.py
+++ vehicle.py
@@ -4,5 +4,5 @@ class Vehicle(object):
     
     def __init__(self, registration_number):
         self.registration_number = registration_number
-        Vehicle.count_list.append(self.registration_number)
+        Vehicle.count_list.append(object)
         Vehicle.count = len(self.count_list)
