80e4654c99dc7cb1bcac3843b79787c76985d5ac
Agi Marczinko <katyvaszok@gmail.com>
2016-03-10 06:14:16 +0100

    Counting objects in Vehicle class

--- vehicle.py
+++ vehicle.py
@@ -1,5 +1,8 @@
 class Vehicle(object):
+
+    count_list = []
     
     def __init__(self, registration_number):
         self.registration_number = registration_number
-        
+        Vehicle.count_list.append(self.registration_number)
+        Vehicle.count = len(self.count_list)
